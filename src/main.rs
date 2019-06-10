1905303b25cd5d7b58de4f8321af4181cb638dc5
Yarsan Hoessain <32988993+hyarsan@users.noreply.github.com>
2019-06-10 21:18:27 +0200

    Add the bot files

---
+++ src/main.rs
@@ -0,0 +1,457 @@
+mod utils;
+
+use std::collections::HashSet;
+use std::sync::Arc;
+
+use serenity::framework::standard::{
+    help_commands,
+    macros::{command, group, help},
+    Args, CommandGroup, CommandResult, Delimiter, HelpOptions, StandardFramework,
+};
+
+use serenity::model::{
+    channel::Message,
+    gateway::Ready,
+    guild::{Guild, PartialGuild},
+    id::{GuildId, UserId},
+};
+
+use serenity::prelude::*;
+
+const SEPERATOR: &str = " \u{2022} ";
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, ctx: Context, ready: Ready) {
+        let (name, discrim) = (ready.user.name, ready.user.discriminator);
+        println!("Logged in as {}#{:04}.", name, discrim);
+
+        utils::update_activity(&ctx);
+    }
+
+    fn cached(&self, ctx: Context, _guilds: Vec<GuildId>) {
+        utils::update_activity(&ctx);
+    }
+
+    fn guild_create(&self, ctx: Context, _guild: Guild, is_new: bool) {
+        if is_new {
+            utils::update_activity(&ctx);
+        }
+    }
+
+    fn guild_delete(&self, ctx: Context, _guild: PartialGuild, _full: Option<Arc<RwLock<Guild>>>) {
+        utils::update_activity(&ctx);
+    }
+}
+
+group!({
+    name: "general",
+    options: {},
+    commands: [about, ping, coinflip, dice, nekoslife, zip, ferrissays, pride],
+});
+
+#[help]
+fn help_command(
+    context: &mut Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    help_commands::with_embeds(context, msg, args, help_options, groups, owners)
+}
+
+#[command]
+#[description = "Sends some info about the bot."]
+fn about(ctx: &mut Context, msg: &Message) -> CommandResult {
+    const TITLE: &str = concat!(env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"),);
+
+    const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+
+    let guild_count = utils::get_guild_count(&ctx);
+
+    msg.channel_id.send_message(&ctx, |m| m
+        .embed(|e| e
+            .title(TITLE)
+            .description(DESCRIPTION)
+            .field("Bot Invite", "[Invite](https://discordapp.com/api/oauth2/authorize?client_id=494235198582423552&permissions=8&scope=bot)", true)
+            .footer(|f| f
+                .text(format!("Servers: {}", guild_count))
+            )
+        )
+    )?;
+
+    Ok(())
+}
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx, "Pong!")?;
+
+    Ok(())
+}
+
+#[command]
+#[description = "Flips a coin."]
+fn coinflip(ctx: &mut Context, msg: &Message) -> CommandResult {
+    if rand::random() {
+        msg.channel_id.say(&ctx, "The coin landed on heads.")?;
+    } else {
+        msg.channel_id.say(&ctx, "The coin landed on tails.")?;
+    }
+
+    Ok(())
+}
+
+#[command]
+#[description = "Rolls up to 5 dice."]
+fn dice(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    use serenity::utils::MessageBuilder;
+
+    use rand::{thread_rng, Rng};
+    use utils::Dice;
+
+    let dices_amount = match args.single::<usize>() {
+        Ok(x) if x < 5 && x > 0 => x,
+        _ => 5,
+    };
+
+    let mut dices = Vec::with_capacity(dices_amount);
+
+    let mut rng = thread_rng();
+    for _ in 0..dices_amount {
+        dices.push(rng.gen::<Dice>())
+    }
+
+    let dices_iter = dices.iter();
+
+    let mut output = MessageBuilder::new();
+
+    for dice in dices_iter.clone() {
+        output.push(&dice);
+    }
+
+    if dices_amount > 1 {
+        output.push("\nThe numbers are: ");
+    } else {
+        output.push("\nThe number is: ");
+    }
+
+    for (i, dice) in dices_iter.clone().enumerate() {
+        if i != (dices_amount - 1) {
+            output.push_bold(dice.clone() as u8);
+
+            if dices_amount != 2 {
+                output.push(", ");
+            } else {
+                output.push(" ");
+            }
+        } else {
+            if dices_amount > 1 {
+                output.push("and ");
+            }
+
+            output.push_bold(dice.clone() as u8);
+            output.push(".\n");
+        }
+    }
+
+    if dices_amount > 1 {
+        let sum: u8 = dices_iter.map(|d| d.clone() as u8).sum();
+        output.push("The total is: ");
+        output.push_bold(sum);
+        output.push(".");
+    }
+
+    msg.channel_id.say(&ctx, output)?;
+
+    Ok(())
+}
+
+#[command]
+#[description = "Sends a `.zip` archive containing the attachments."]
+fn zip(ctx: &mut Context, msg: &Message) -> CommandResult {
+    use std::io::{Cursor, Write};
+    use zip::{write::FileOptions, ZipWriter};
+
+    msg.channel_id.broadcast_typing(&ctx)?;
+
+    let mut zip = ZipWriter::new(Cursor::new(Vec::new()));
+
+    for attachment in msg.attachments.iter() {
+        zip.start_file(&*attachment.filename, FileOptions::default())?;
+        zip.write(&attachment.download()?)?;
+    }
+
+    msg.channel_id.send_files(
+        &ctx,
+        vec![(zip.finish()?.into_inner().as_slice(), "output.zip")],
+        |m| m,
+    )?;
+
+    Ok(())
+}
+
+/*
+#[command]
+fn mcskin(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
+    use percent_encoding::percent_decode;
+
+    if let Some(username) = args.current() {
+        let json: serde_json::Value = reqwest::get(&format!("https://api.mojang.com/users/profiles/minecraft/{}", username))
+            .expect("couldn't retrieve user profile")
+            .json()?;
+
+        let uuid = json["id"].as_str().unwrap();
+        let username = json["name"].as_str().unwrap();
+
+        let json: serde_json::Value = reqwest::get(&format!("https://sessionserver.mojang.com/session/minecraft/profile/{}", uuid))
+            .expect("couldn't retrieve user profile")
+            .json()?;
+
+        let base64_value = json["properties"][0]["value"].as_str().unwrap();
+        let json = base64::decode(base64_value).unwrap();
+        let json = percent_decode(json.as_slice()).decode_utf8().unwrap();
+
+        msg.channel_id.send_message(&ctx, |m| m
+            .embed(|e| e
+                .description(format!("{}", json))
+                .footer(|f| f.text(username))
+            )
+        )?;
+    } else {
+        msg.channel_id.say(&ctx, "No username provided.")?;
+    }
+
+    Ok(())
+}
+*/
+
+#[command]
+#[aliases("nl")]
+fn nekoslife(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    use itertools::Itertools;
+    use nekos::{self, ImageCategory};
+    use random_color::{Luminosity, RandomColor};
+
+    if let Some(category) = args.current() {
+        if let Ok(category) = category.parse::<ImageCategory>() {
+            if category.is_sfw() || msg.channel_id.to_channel(&ctx)?.is_nsfw() {
+                let client = nekos::Client::new();
+
+                msg.channel_id.broadcast_typing(&ctx)?;
+
+                args.advance();
+                if let Ok(amount) = args.single::<u8>() {
+                    let mut output = String::new();
+                    for _ in 0..amount {
+                        output += &client.get_random_image(category.clone())?.url;
+                        output += "\n";
+                    }
+
+                    msg.channel_id.say(&ctx, output)?;
+                } else {
+                    let image = client.get_random_image(category.clone())?;
+
+                    let embed_color =
+                        utils::gen_random_color(RandomColor::new().luminosity(Luminosity::Light));
+
+                    msg.channel_id.send_message(&ctx, |m| {
+                        m.embed(|e| {
+                            e.footer(|f| f.text(format!("Category: {}", category)))
+                                .image(image.url)
+                                .colour(embed_color)
+                        })
+                    })?;
+                }
+            } else {
+                msg.channel_id.say(
+                    &ctx,
+                    "The image category isn't SFW, so you have to be in an NSFW channel to use it.",
+                )?;
+            }
+        } else {
+            msg.channel_id
+                .say(&ctx, "Invalid image category specified.")?;
+        }
+    } else {
+        let client = nekos::Client::new();
+
+        let mut categories = client.get_image_categories()?;
+        if !msg.channel_id.to_channel(&ctx)?.is_nsfw() {
+            categories.retain(|c| c.is_sfw());
+        }
+
+        let description = categories.iter().join(SEPERATOR);
+
+        let embed_color = utils::gen_random_color(RandomColor::new().luminosity(Luminosity::Light));
+
+        msg.channel_id.send_message(&ctx, |m| {
+            m.embed(|e| {
+                e.title("Available Image Categories")
+                    .description(description)
+                    .colour(embed_color)
+            })
+        })?;
+    }
+
+    Ok(())
+}
+
+#[command]
+#[description = "Makes Ferris the Crab say something."]
+fn ferrissays(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
+    use serenity::utils::MessageBuilder;
+
+    let mut output = MessageBuilder::new();
+    let mut ferris = Vec::new();
+
+    ferris_says::say(args.rest().as_bytes(), 24, &mut ferris)?;
+    output.push_codeblock(String::from_utf8(ferris)?, None);
+
+    msg.channel_id.say(&ctx, output)?;
+
+    Ok(())
+}
+
+/*
+#[command]
+#[description = "Outputs the sum of the numbers passed in."]
+fn sum(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    use itertools::Itertools;
+
+    let iter = args.iter::<f64>().filter_map(|n| n.ok());
+
+    let foo = iter.join(" + ");
+    let sum: f64 = iter.sum();
+
+    msg.channel_id.say(&ctx, format!("{} = {}", foo, sum))?;
+
+    Ok(())
+}
+*/
+
+#[command]
+#[description = "Makes your avatar all the more gay! 🏳️‍🌈"]
+fn pride(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    use crate::utils::PrideFlag;
+
+    use serenity::http::AttachmentType;
+
+    use image::{
+        imageops, png::PNGEncoder, ColorType, DynamicImage, FilterType, GenericImage,
+        GenericImageView, ImageFormat,
+    };
+
+    // random is imported for a random color of the pride flag
+    use rand::{seq::SliceRandom, thread_rng};
+
+    use lenient_bool::LenientBool;
+
+    msg.channel_id.broadcast_typing(&ctx)?;
+
+    let flag: PrideFlag = args.single().unwrap_or_default();
+
+    let filter = match args.single::<LenientBool>() {
+        Ok(lb) => lb.into(),
+        _ => false,
+    };
+    let filter = if filter {
+        FilterType::Gaussian
+    } else {
+        FilterType::Nearest
+    };
+
+    let a = match args.single::<u8>() {
+        Ok(percent) if percent >= 20 && percent <= 80 => {
+            ((percent as f64 / 100.0) * 255.0).trunc() as u8
+        }
+        _ => 127,
+    };
+
+    let mut image_bytes = vec![];
+    reqwest::get(&msg.author.static_avatar_url().unwrap())?.copy_to(&mut image_bytes)?;
+
+    let mut image = image::load_from_memory_with_format(&image_bytes, ImageFormat::WEBP)?.to_rgba();
+
+    let mut prideflag_image = DynamicImage::new_rgba8(1, flag.colors().iter().count() as u32);
+    for (i, color) in flag.colors().iter().enumerate() {
+        let (r, g, b) = (
+            ((*color >> 16) & 255) as u8,
+            ((*color >> 8) & 255) as u8,
+            (*color & 255) as u8,
+        );
+
+        unsafe {
+            prideflag_image.unsafe_put_pixel(0, i as u32, image::Rgba([r, g, b, a]));
+        }
+    }
+
+    let prideflag_image = prideflag_image.resize_exact(image.width(), image.height(), filter);
+    imageops::overlay(&mut image, &prideflag_image, 0, 0);
+
+    let mut output_bytes = vec![];
+    PNGEncoder::new(&mut output_bytes).encode(
+        &image.into_raw(),
+        prideflag_image.width(),
+        prideflag_image.height(),
+        ColorType::RGBA(8),
+    )?;
+
+    msg.channel_id.send_message(&ctx, |m| {
+        m.embed(|e| {
+            e.footer(|f| f.text(&flag))
+                .colour(*flag.colors().choose(&mut thread_rng()).unwrap())
+                .image("attachment://prideified.png")
+        });
+        m.add_file(AttachmentType::Bytes((&output_bytes, "prideified.png")));
+        m
+    })?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let config = utils::read_config()?;
+
+    let mut client = Client::new(&config.token, Handler)?;
+
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| {
+                c.prefix(&config.prefix.unwrap_or(String::from(":-")))
+                    .on_mention(Some(serenity::model::id::UserId(494_235_198_582_423_552)))
+                    .no_dm_prefix(true)
+            })
+            .group(&GENERAL_GROUP)
+            .help(&HELP_COMMAND_HELP_COMMAND)
+            .prefix_only(|ctx, msg| {
+                about(ctx, msg, Args::new("", &[Delimiter::Single(' ')]))
+                    .expect("couldn't run the about command");
+            })
+            .after(|ctx, msg, command_name, command_result| {
+                use serenity::utils::Colour;
+
+                if let Err(command_error) = command_result {
+                    msg.channel_id
+                        .send_message(&ctx, |m| {
+                            m.embed(|e| {
+                                e.title("An error occurred")
+                                    .description(format!("```{}```", command_error.0))
+                                    .footer(|f| f.text(format!("Command: {}", command_name)))
+                                    .colour(Colour::RED)
+                            })
+                        })
+                        .expect("couldn't send message");
+                }
+            }),
+    );
+
+    if let Err(e) = client.start() {
+        eprintln!("client error: {:?}", e);
+    }
+
+    Ok(())
+}
---
+++ src/utils.rs
@@ -0,0 +1,202 @@
+use std::error;
+use std::str::FromStr;
+use std::{fmt, fs};
+
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+
+use serenity::model::gateway::Activity;
+use serenity::prelude::*;
+use serenity::utils::Colour;
+
+use random_color::RandomColor;
+
+use serde::Deserialize;
+
+#[derive(Debug, Clone)]
+pub enum Dice {
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+}
+
+impl Distribution<Dice> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Dice {
+        match rng.gen_range(0, 6) {
+            0 => Dice::One,
+            1 => Dice::Two,
+            2 => Dice::Three,
+            3 => Dice::Four,
+            4 => Dice::Five,
+            _ => Dice::Six,
+        }
+    }
+}
+
+impl fmt::Display for Dice {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.pad(match self {
+            Dice::One => "<:dice_1:501061465265143838>",
+            Dice::Two => "<:dice_2:501061466552795137>",
+            Dice::Three => "<:dice_3:501061466435092483>",
+            Dice::Four => "<:dice_4:501061466900660233>",
+            Dice::Five => "<:dice_5:501061467072888855>",
+            Dice::Six => "<:dice_6:501061466926088202>",
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum PrideFlag {
+    Rainbow,
+    Lesbian,
+    Bi,
+    Pan,
+    Poly,
+    Trans,
+    Genderqueer,
+    NonBinary,
+    Genderfluid,
+    Agender,
+    Neutrois,
+    Asexual,
+    Aromantic,
+}
+
+impl PrideFlag {
+    pub fn colors(&self) -> &'static [u32] {
+        use crate::utils::PrideFlag::*;
+
+        match self {
+            Rainbow => &[0xE40303, 0xFF8C00, 0xFFED00, 0x008026, 0x004DFF, 0x750787],
+            Lesbian => &[
+                // 0xA40061, 0xB75592, 0xD063A6, 0xEDEDEB, 0xE4ACCF, 0xC54E54, 0x8A1E04,
+                0xD62C00, /*0xF07528,*/ 0xFF9B57, 0xEDEDEB, 0xD162A6,
+                /*0xB75592,*/ 0xA50162,
+            ],
+            Bi => &[0xD60270, 0xD60270, 0x9B4F96, 0x0038A8, 0x0038A8],
+            Pan => &[0xFF218C, 0xFFD800, 0x21B1FF],
+            Poly => &[0xF61CB9, 0x07D569, 0x1C92F6],
+            Trans => &[0x5BCEFA, 0xF5A9B8, 0xFFFFFF, 0xF5A9B8, 0x5BCEFA],
+            Genderqueer => &[0xB57EDC, 0xFFFFFF, 0x4A8123],
+            NonBinary => &[0xFFF42C, 0xFFFFFF, 0x9D59D2, 0x000000],
+            Genderfluid => &[0xFF75A2, 0xFFFFFF, 0xBE18D6, 0x000000, 0x333EBD],
+            Agender => &[
+                0x000000, 0xB9B9B9, 0xFFFFFF, 0xB8F483, 0xFFFFFF, 0xB9B9B9, 0x000000,
+            ],
+            Neutrois => &[0xFFFFFF, 0x22B14C, 0x000000],
+            Asexual => &[0x000000, 0xA3A3A3, 0xFFFFFF, 0x800080],
+            Aromantic => &[0x3DA542, 0xA7D379, 0xFFFFFF, 0xA9A9A9, 0x000000],
+        }
+    }
+}
+
+impl FromStr for PrideFlag {
+    type Err = ParsePrideFlagError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use crate::utils::PrideFlag::*;
+
+        match &s.to_lowercase()[..] {
+            "gay" | "queer" | "rainbow" | "lgbt" | "lgbtq+" => Ok(Rainbow),
+            "les" | "lesbian" | "lesbean" => Ok(Lesbian),
+            "bi" | "bisexual" => Ok(Bi),
+            "pan" | "pansexual" => Ok(Pan),
+            "poly" | "polysexual" => Ok(Poly),
+            "trans" | "transgender" | "icecream" => Ok(Trans),
+            "genderqueer" => Ok(Genderqueer),
+            "enby" | "nb" | "nonbinary" | "non-binary" => Ok(NonBinary),
+            "genderfluid" => Ok(Genderfluid),
+            "agender" => Ok(Agender),
+            "neutrois" => Ok(Neutrois),
+            "ace" | "asexual" => Ok(Asexual),
+            "aromantic" => Ok(Aromantic),
+
+            _ => Err(Self::Err {}),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ParsePrideFlagError;
+
+impl fmt::Display for ParsePrideFlagError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "provided string wasn't a valid pride flag")
+    }
+}
+
+impl error::Error for ParsePrideFlagError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
+
+impl fmt::Display for PrideFlag {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use crate::utils::PrideFlag::*;
+
+        f.pad(match self {
+            Rainbow => "rainbow pride flag",
+            Lesbian => "lesbian pride flag",
+            Bi => "bisexual pride flag",
+            Pan => "pansexual pride flag",
+            Poly => "polysexual pride flag",
+            Trans => "transgender pride flag",
+            Genderqueer => "genderqueer pride flag",
+            NonBinary => "non-binary pride flag",
+            Genderfluid => "genderfluid pride flag",
+            Agender => "agender pride flag",
+            Neutrois => "neutrois pride flag",
+            Asexual => "asexual pride flag",
+            Aromantic => "aromantic pride flag",
+        })
+    }
+}
+
+impl Default for PrideFlag {
+    #[inline]
+    fn default() -> PrideFlag {
+        PrideFlag::Rainbow
+    }
+}
+
+/// Represents the bot's config.
+#[derive(Deserialize)]
+pub struct Config {
+    pub token: String,
+    pub prefix: Option<String>,
+}
+
+/// Reads the bot's configuration file.
+pub fn read_config() -> Result<Config, Box<std::error::Error>> {
+    let contents = fs::read_to_string("config.toml")?;
+
+    let config: Config = toml::from_str(&contents)?;
+
+    Ok(config)
+}
+
+/// Gets the bot's guild count.
+pub fn get_guild_count(ctx: &Context) -> usize {
+    ctx.cache.read().guilds.len()
+}
+
+/// Updates the bot's activity.
+pub fn update_activity(ctx: &Context) {
+    let guild_count = get_guild_count(ctx);
+
+    ctx.set_activity(Activity::listening(&format!("{} servers", guild_count)));
+}
+
+/// Generates a random `serenity::utils::Colour` from a `random_color::RandomColor`.
+pub fn gen_random_color(color: &mut RandomColor) -> Colour {
+    let [r, g, b] = color.to_rgb_array();
+
+    (r as u8, g as u8, b as u8).into()
+}
